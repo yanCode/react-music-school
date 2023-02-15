cf7f084dfe4b0e0b61551eabe38bf2d2311ae083
ShengYan, Zhang <yan.code@gmail.com>
2023-02-15 12:43:11 +0800

    feat: add Topics section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,19 @@
 import student from './student.svg'
 import heroBackground from './hero-background.png'
+import topic_01 from './topics/01.png';
+import topic_02 from './topics/02.png';
+import topic_03 from './topics/03.png';
+import topic_04 from './topics/04.png';
+import topic_05 from './topics/05.png';
+import yellowCorner from './yellow_corner.svg'
 
-export {student, heroBackground}
+export {
+  student,
+  heroBackground,
+  topic_01,
+  topic_02,
+  topic_03,
+  topic_04,
+  topic_05,
+  yellowCorner
+}
--- src/sences/Topics/index.jsx
+++ src/sences/Topics/index.jsx
@@ -1,4 +1,28 @@
 import './Topics.css'
-export const Topics = ()=>{
-  return (<div>Navbar</div>)
+import {topic_01, topic_02, topic_03, topic_04, topic_05, yellowCorner} from "../../assets";
+import {useState} from "react";
+
+export const Topics = () => {
+  const [currentImage, setCurrentImage] = useState(topic_01);
+  return (<section id="topics" className="background-black">
+    <div className='topic__wrapper'>
+      <div className="wrapper">
+        <h2>What will you learn?</h2>
+        <div className='content__container'>
+           <ul className='topic__list'>
+             <li onMouseEnter={()=>setCurrentImage(topic_01)}>What are frequencies?</li>
+             <li onMouseEnter={()=>setCurrentImage(topic_02)}>Using DAW</li>
+             <li onMouseEnter={()=>setCurrentImage(topic_03)}>Vocals Processing</li>
+             <li onMouseEnter={()=>setCurrentImage(topic_04)}>Mixing</li>
+             <li onMouseEnter={()=>setCurrentImage(topic_05)}>Mixing Console</li>
+             <li onMouseEnter={()=>setCurrentImage(topic_04)}>Mastering</li>
+           </ul>
+          <div className='topic__image'>
+            <img src={currentImage} alt= 'topic_image'/>
+          </div>
+        </div>
+        <img src={yellowCorner} alt="right corner" className='yellow-bg-corner'/>
+      </div>
+    </div>
+  </section>)
 }
