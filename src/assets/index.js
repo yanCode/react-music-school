47fbad166714eef78032ed70257d703185f98d15
ShengYan, Zhang <yan.code@gmail.com>
2023-02-15 13:13:43 +0800

    feat: add Info section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import student from './student.svg'
+import file from './file.svg'
 import heroBackground from './hero-background.png'
 import topic_01 from './topics/01.png';
 import topic_02 from './topics/02.png';
@@ -6,14 +7,17 @@ import topic_03 from './topics/03.png';
 import topic_04 from './topics/04.png';
 import topic_05 from './topics/05.png';
 import yellowCorner from './yellow_corner.svg'
+import redCorner from './red_corner.svg'
 
 export {
   student,
+  file,
   heroBackground,
   topic_01,
   topic_02,
   topic_03,
   topic_04,
   topic_05,
-  yellowCorner
+  yellowCorner,
+  redCorner
 }
--- src/sences/Info/index.jsx
+++ src/sences/Info/index.jsx
@@ -1,4 +1,22 @@
+import './Info.css'
+import {file, redCorner, student} from "../../assets";
 
-export const Info = ()=>{
-  return (<div>Info</div>)
+export const Info = () => {
+  return (<section id="info" className="background-dark-grey">
+    <div className='wrapper'>
+      <div className='info_container'>
+        <div className='info-content'>
+          <img src={student} alt='student icon'/>
+          <div className='amount'>23,000+</div>
+          <div className="type">Students</div>
+        </div>
+        <div className='info-content'>
+          <img src={file} alt='file icon'/>
+          <div className='amount'>26 Hrs</div>
+          <div className="type">Video Content</div>
+        </div>
+      </div>
+      <img className="red-corner" src={redCorner} alt="red corner"/>
+    </div>
+  </section>)
 }
