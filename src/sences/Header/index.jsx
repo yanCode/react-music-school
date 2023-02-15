68a0c6e100905600110202609c1a5b27e39d615d
ShengYan, Zhang <yan.code@gmail.com>
2023-02-15 10:53:55 +0800

    feat: add nav-bar

--- src/sences/Header/index.jsx
+++ src/sences/Header/index.jsx
@@ -1,9 +1,11 @@
 import './Header.css'
+import {Navbar} from "../Navbar/index.jsx";
 
 export const Header = () => {
   return (
     <header>
       <div className='wrapper'>
+        <Navbar/>
         <div className="cta" >
           <p className="course-name">Sound Design Masterclass</p>
           <h1>Learn the Art of Sound Design</h1>
--- src/sences/Navbar/index.jsx
+++ src/sences/Navbar/index.jsx
@@ -1,4 +1,15 @@
 import './Navbar.css'
-export const Navbar = ()=>{
-  return (<div>Navbar</div>)
+
+export const Navbar = () => {
+  return (<nav className='nav__bar'>
+    <a href="" className="logo">
+      Sound<span className="red">DZign</span>
+    </a>
+    <ul>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Course Details</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">Testimonials</a></li>
+    </ul>
+  </nav>)
 }
